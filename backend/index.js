cef6c1ed51b7b751d3367d8428aa1a206821637c
Abhiram Yenugadhati <abhiramyenugadhati@gmail.comgit>
2022-04-09 15:20:52 -0700

    Image Upload

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const fileStorageEngine = multer.diskStorage({
     cb(null, file.fieldname + ".jpeg");
   },
 });
-
+//image Upload
 const upload = multer({ storage: fileStorageEngine });
 
 app.use(express.static("public"));
